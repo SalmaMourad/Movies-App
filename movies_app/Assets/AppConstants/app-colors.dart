8e469a7c7ce5591695d7280600effe4d227270d9
SalmaMourad <salmamourad992@gmail.com>
2025-03-14 08:14:30 +0200

    project Initial setup

---
+++ movies_app/Assets/AppConstants/app-colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Appcolors {
+
+  static const yellowColor=Color(0xFFF6BD00);
+  static const blackColor=Color(0xFF121312);
+  static const greyColor=Color(0xFF282A28);
+  static const redColor=Color(0xFFE82626);
+  static const whiteColor=Color(0xFFFFFFFF);
+}
---
+++ movies_app/Assets/AppConstants/app-images.dart
@@ -0,0 +1,5 @@
+class AppAssets {
+  static const String _imagesDir = 'movies_app/Assets/Images';
+  static const String ImageName = '$_imagesDir/image.png'; //example
+ 
+}
