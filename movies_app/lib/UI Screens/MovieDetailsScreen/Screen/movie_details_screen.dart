1a743e18782ffbfa167a88d89a279ae4661e0ed8
SalmaMourad <salmamourad992@gmail.com>
2025-03-21 03:22:02 +0200

    .

--- movies_app/lib/UI Screens/MovieDetailsScreen/Screen/movie_details_screen.dart
+++ movies_app/lib/UI Screens/MovieDetailsScreen/Screen/movie_details_screen.dart
@@ -6,10 +6,10 @@ import '../Widgets/SimilarMoviesContainer.dart';
 import '../Widgets/SummaryContainer.dart';
 import '../Widgets/genere.dart';
 import '../Widgets/watchCustomButton.dart';
-import '../Widgets/MovieCastContainer.dart'; // Import the new cast widget
+import '../Widgets/MovieCastContainer.dart'; 
 
 class MovieDetailsScreen extends StatelessWidget {
-  final List<Map<String, String>> allMovies; // For testing
+  final List<Map<String, String>> allMovies; 
   final List<Map<String, String>> castList;
 
   MovieDetailsScreen(
@@ -28,7 +28,7 @@ class MovieDetailsScreen extends StatelessWidget {
             ScreenShotsContainer(),
             SimilarMoviesContainer(allMovies: allMovies),
             SummaryContainer(),
-            MovieCastContainer(castList: castList), // Add the Cast Widget here
+            MovieCastContainer(castList: castList), 
             MovieGenere(),
           ],
         ),
@@ -36,36 +36,3 @@ class MovieDetailsScreen extends StatelessWidget {
     );
   }
 }
-
-// import 'package:flutter/material.dart';
-// import '../Widgets/MoviePosterDetailsContainer.dart';
-// import '../Widgets/RatingsAllContainers.dart';
-// import '../Widgets/ScreenShotsContainer.dart';
-// import '../Widgets/SimilarMoviesContainer.dart';
-// import '../Widgets/SummaryContainer.dart';
-// import '../Widgets/watchCustomButton.dart';
-
-// class MovieDetailsScreen extends StatelessWidget {
-//   final List<Map<String, String>> allMovies; //34an 23ml test bs
-
-//   MovieDetailsScreen({super.key, required this.allMovies});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Color(0xFF121312),
-//       body: SingleChildScrollView(
-//         child: Column(
-//           children: [
-//             MoviePosterDetailsContainer(),
-//             watchCustomButton(),
-//             RatingsAllContainers(),
-//             ScreenShotsContainer(),
-//             SimilarMoviesContainer(allMovies: allMovies),
-//             SummaryContainer()
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
